a9be53e2f957f5755b3c32adeaf4affda479920b
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-01-30 11:47:55 -0800

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,121 @@
+let searchInput = document.getElementById("searchInput");
+let searchBtn = document.getElementById("searchBtn");
+
+let normalFront = document.getElementById("normalFront");
+let shinyFront = document.getElementById("shinyFront");
+let namePoke = document.getElementById("namePoke");
+let findLocation = document.getElementById("findLocation");
+let eleType = document.getElementById("eleType");
+let abilities = document.getElementById("abilities");
+let movesPoke = document.getElementById("movesPoke");
+let evol = document.getElementById("evol");
+let favBtn = document.getElementById("favBtn");
+let openFav = document.getElementById("openFav");
+
+let savedPokemonArray = [];
+
+if (localStorage.getItem("Pokemon")) {
+    savedPokemonArray = JSON.parse(localStorage.getItem("Pokemon"));
+};
+
+const FetchPoke = async (input) => {
+    const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`);
+    const data = await promise.json();
+
+    console.log(data);
+    PopPoke(data);
+};
+
+searchBtn.addEventListener('click', () => {
+    FetchPoke(searchInput.value.toLowerCase());
+});
+
+const PopPoke = async (data) => {
+    normalFront.src = data.sprites.front_default;
+    shinyFront.src = data.sprites.front_shiny;
+
+    namePoke.innerText = data.name[0].toUpperCase() + data.name.substring(1);
+
+    popLocate(data);
+
+    for (let i = 0; i < data.types.length; i++) {
+        switch (i) {
+            case 0:
+                eleType.innerText = data.types[i].type.name[0].toUpperCase() + data.types[i].type.name.substring(1);
+                break;
+            default:
+                eleType.innerText += ", " + data.types[i].type.name[0].toUpperCase() + data.types[i].type.name.substring(1);
+                break;
+        };
+    };
+
+    for (let i = 0; i < data.abilities.length; i++) {
+        switch (i) {
+            case 0:
+                abilities.innerText = data.abilities[i].ability.name[0].toUpperCase() + data.abilities[i].ability.name.substring(1);
+                break;
+            default:
+                abilities.innerText += ", " + data.abilities[i].ability.name[0].toUpperCase() + data.abilities[i].ability.name.substring(1);
+                break;
+        };
+    };
+
+    for (let i = 0; i < data.moves.length; i++) {
+        switch (i) {
+            case 0:
+                movesPoke.innerText = data.moves[i].move.name[0].toUpperCase() + data.moves[i].move.name.substring(1);
+                break;
+            default:
+                movesPoke.innerText += ", " + data.moves[i].move.name[0].toUpperCase() + data.moves[i].move.name.substring(1);
+                break;
+        };
+    };
+
+    popEvol(data);
+};
+
+const popLocate = async (topData) => {
+    const promise = await fetch(`${topData.location_area_encounters}`);
+    const data = await promise.json();
+
+    let locationName = data[0].location_area.name.split("-").join(" ");
+    let versionName = data[0].version_details[0].version.name[0].toUpperCase() + data[0].version_details[0].version.name.substring(1);
+
+    // for (let i = 0; i < locationName.length; i++){
+    //     location[i] = locationName[i][0].toUpperCase() + locationName[i].substring(1);
+    //     console.log(locationName);
+
+    // }
+
+    findLocation.innerText = locationName + ", Pokemon " + versionName;
+};
+
+const popEvol = async (topData) => {
+    const promiseOne = await fetch(`${topData.species.url}`);
+    const dataOne = await promiseOne.json();
+
+    const promiseTwo = await fetch(`${dataOne.evolution_chain.url}`);
+    const dataTwo = await promiseTwo.json();
+
+
+    // evol.innerText = dataTwo.chain.species.name;
+    const valueForThree = dataTwo?.chain?.evolves_to[0]?.evolves_to[0]?.species?.name;
+    if(valueForThree !== undefined){
+        evol.innerText = dataTwo.chain.species.name + ", " + dataTwo.chain.evolves_to[0].species.name + ", " + dataTwo.chain.evolves_to[0].evolves_to[0].species.name;
+    }else{
+        const valueForTwo = dataTwo?.chain?.evolves_to[0]?.species?.name;
+        if(valueForTwo !== undefined){
+            // if(1 < dataTwo.chain.evolves_to.length){
+            //     for
+            // }
+
+            evol.innerText = dataTwo.chain.species.name;
+
+            for(let i = 0; i < dataTwo.chain.evolves_to.length; i++){
+                evol.innerText += ", " + dataTwo.chain.evolves_to[i].species.name;
+            }
+        }else{
+            evol.innerText = "N/A"
+        }
+    }
+};
